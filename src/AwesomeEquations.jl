06cc039cf33e37fa990bd22838efb94888bf232c
Syver Døving Agdestein <syverda@icloud.com>
2022-09-13 10:43:45 +0200

    Fix solver

--- src/AwesomeEquations.jl
+++ src/AwesomeEquations.jl
@@ -9,8 +9,10 @@ function forward_euler(f, u0, T, dt = 0.1)
     u = u0
     n = round(Int, T / dt)
     dt = T / n
+    t = 0.0
     for i = 1:n
         u = u + dt * f(u, t)
+        t + dt
     end
     return u
 end
