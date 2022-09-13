7f0e144c8eb0a6daf7e954d74272cb95a7bf6803
Syver Døving Agdestein <syverda@gmail.com>
2022-09-13 13:24:48 +0200

    Add parameterized solver

--- src/solvers.jl
+++ src/solvers.jl
@@ -1,35 +1,36 @@
 """
-    forward_euler(f, u0, T, Δt = 0.1)
+    forward_euler(f, p, u₀, T, Δt = 0.1)
 
-Solve ODE ``du/dt = f(u, t)`` with forward Euler.
+Solve ODE ``du/dt = f(u, p, t)`` with forward Euler, where ``p`` are
+parameters.
 """
-function forward_euler(f, u₀, T, Δt = 0.1)
+function forward_euler(f, p, u₀, T, Δt = 0.1)
     u = u₀
     n = round(Int, T / Δt)
     Δt = T / n
     t = 0.0
     for i = 1:n
-        u = u + Δt * f(u, t)
+        u = u + Δt * f(u, p, t)
         t = t + Δt
     end
     u
 end
 
 """
-    rk4(f, u0, T, Δt = 0.1)
+    rk4(f, p, u₀, T, Δt = 0.1)
 
-Solve ODE ``du/dt = f(u, t)`` with RK4.
+Solve ODE ``du/dt = f(u, p, t)`` with RK4, where ``p`` are parameters.
 """
-function rk4(f, u₀, T, Δt = 0.1)
+function rk4(f, p, u₀, T, Δt = 0.1)
     u = u₀
     n = round(Int, T / Δt)
     Δt = T / n
     t = 0.0
     for i = 1:n
-        k₁ = f(u, t)
-        k₂ = f(u + Δt * k₁, t + Δt / 2)
-        k₃ = f(u + Δt * k₂, t + Δt / 2)
-        k₄ = f(u + Δt * k₃, t + Δt)
+        k₁ = f(u, p, t)
+        k₂ = f(u + Δt * k₁, p, t + Δt / 2)
+        k₃ = f(u + Δt * k₂, p, t + Δt / 2)
+        k₄ = f(u + Δt * k₃, p, t + Δt)
         u = u + Δt * (k₁ / 6 + k₂ / 3 + k₃ / 3 + k₄ / 6)
         t = t + Δt
     end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,26 +3,27 @@ using Test
 
 @testset "AwesomeEquations.jl" begin
     @testset "ODE solvers" begin
-        f(u, t) = u
-        g(u, t) = -t * u
+        f = (u, p, t) -> u
+        g = (u, p, t) -> -t * u
 
         u₀ = 2.0
         t = 3.0
         u_f = exp(t) * u0
         u_g = exp(-t^2 / 2) * u0
+        p = nothing
 
         @testset "Forward Euler" begin
-            @test forward_euler(f, u₀, t, 1e-4) ≈ u_f rtol = 1e-3
-            @test forward_euler(f, u₀, t, 1e-5) ≈ u_f rtol = 1e-4
-            @test forward_euler(g, u₀, t, 1e-4) ≈ u_g rtol = 1e-3
-            @test forward_euler(g, u₀, t, 1e-5) ≈ u_g rtol = 1e-4
+            @test forward_euler(f, p, u₀, t, 1e-4) ≈ u_f rtol = 1e-3
+            @test forward_euler(f, p, u₀, t, 1e-5) ≈ u_f rtol = 1e-4
+            @test forward_euler(g, p, u₀, t, 1e-4) ≈ u_g rtol = 1e-3
+            @test forward_euler(g, p, u₀, t, 1e-5) ≈ u_g rtol = 1e-4
         end
 
         @testset "RK4" begin
-            @test rk4(f, u₀, t, 1e-3) ≈ u_f rtol = 1e-3
-            @test rk4(f, u₀, t, 1e-4) ≈ u_f rtol = 1e-4
-            @test rk4(g, u₀, t, 1e-3) ≈ u_g rtol = 1e-2
-            @test rk4(g, u₀, t, 1e-4) ≈ u_g rtol = 1e-3
+            @test rk4(f, p, u₀, t, 1e-3) ≈ u_f rtol = 1e-3
+            @test rk4(f, p, u₀, t, 1e-4) ≈ u_f rtol = 1e-4
+            @test rk4(g, p, u₀, t, 1e-3) ≈ u_g rtol = 1e-2
+            @test rk4(g, p, u₀, t, 1e-4) ≈ u_g rtol = 1e-3
         end
     end
 
@@ -36,7 +37,24 @@ using Test
         @test forward_diff(exp, 1.0) ≈ exp(1.0) rtol = 1e-8
 
         # Check that chain rule works correctly
-        h(x) = exp(2 * sin(3x)^(1 / x + 4))
-        @test forward_diff(h, 1.0) ≈ finite_diff(h, 1.0) rtol = 1e-7
+        f = x -> exp(2 * sin(3x)^(1 / x + 4))
+        @test forward_diff(f, 1.0) ≈ finite_diff(f, 1.0) rtol = 1e-7
+    end
+
+    @testset "ODE differentiation" begin
+        # Solve parameterized exponential equation ``du/dt = -p u``.
+        # The solution is given by ``u(t) = \\exp(-p t) u``
+        f = (u, p, t) -> -p * u 
+        u₀ = 2.0
+        t = 3.0
+        p = 1.3
+
+        # Differentiate forward Euler
+        solve = p -> forward_euler(f, u₀, p, t)
+        @test forward_diff(solve, p) ≈ finite_diff(solve, p) rtol = 1e-8
+
+        # Differentiate RK4
+        solve = p -> rk4(f, u₀, p, t)
+        @test forward_diff(solve, p) ≈ finite_diff(solve, p) rtol = 1e-7
     end
 end
