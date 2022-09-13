b39f176943319ecac7887fe08716143208a87621
Syver Døving Agdestein <syverda@icloud.com>
2022-09-13 10:39:12 +0200

    Add forward Euler solver

--- src/AwesomeEquations.jl
+++ src/AwesomeEquations.jl
@@ -1,5 +1,20 @@
 module AwesomeEquations
 
-# Write your package code here.
+"""
+    forward_euler(f, u0, T, dt = 0.1)
+
+Solve ODE ``du/dt = f(u, t)``
+"""
+function forward_euler(f, u0, T, dt = 0.1)
+    u = u0
+    n = round(Int, T / dt)
+    dt = T / n
+    for i = 1:n
+        u = u + dt * f(u, t)
+    end
+    return u
+end
+
+export forward_euler
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,10 @@ using AwesomeEquations
 using Test
 
 @testset "AwesomeEquations.jl" begin
-    # Write your tests here.
+    f(u, t) = u
+    u0 = 1.0
+    T = 5.0
+    u = forward_euler(f, u0, T, 0.01)
+    u_ref = exp(T) * u0
+    @test u ≈ u_ref atol = 1e-3
 end
