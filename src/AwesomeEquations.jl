41a5fdd230b49b0753502454ad7fb27ae30af256
Syver Døving Agdestein <syverda@icloud.com>
2022-09-13 10:52:11 +0200

    Add RK4

--- src/AwesomeEquations.jl
+++ src/AwesomeEquations.jl
@@ -3,7 +3,7 @@ module AwesomeEquations
 """
     forward_euler(f, u0, T, dt = 0.1)
 
-Solve ODE ``du/dt = f(u, t)``
+Solve ODE ``du/dt = f(u, t)`` with forward Euler.
 """
 function forward_euler(f, u0, T, dt = 0.1)
     u = u0
@@ -17,6 +17,27 @@ function forward_euler(f, u0, T, dt = 0.1)
     return u
 end
 
-export forward_euler
+"""
+    rk4(f, u0, T, dt = 0.1)
+
+Solve ODE ``du/dt = f(u, t)`` with RK4.
+"""
+function rk4(f, u0, T, dt = 0.1)
+    u = u0
+    n = round(Int, T / dt)
+    dt = T / n
+    t = 0.0
+    for i = 1:n
+        k1 = f(u, t)
+        k2 = f(u + dt * k1, t + dt/2)
+        k3 = f(u + dt * k2, t + dt/2)
+        k4 = f(u + dt * k3, t + dt)
+        u = u + dt * (k1 / 6 + k2 / 3 + k3 / 3 + k4 / 6)
+        t + dt
+    end
+    return u
+end
+
+export forward_euler, rk4
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,9 +3,20 @@ using Test
 
 @testset "AwesomeEquations.jl" begin
     f(u, t) = u
-    u0 = 1.0
-    T = 5.0
-    u = forward_euler(f, u0, T, 0.00001)
-    u_ref = exp(T) * u0
-    @test u ≈ u_ref atol = 1e-2
+
+    @testset "Forward EuleR" begin
+        u0 = 1.0
+        T = 5.0
+        u_ref = exp(T) * u0
+        @test forward_euler(f, u0, T, 0.00001) ≈ u_ref atol = 1e-2
+        @test forward_euler(f, u0, T, 1e-6) ≈ u_ref atol = 1e-3
+    end
+
+    @testset "RK4" begin
+        u0 = 1.0
+        T = 5.0
+        u_ref = exp(T) * u0
+        @test rk4(f, u0, T, 0.001) ≈ u_ref atol = 1e-2
+        @test rk4(f, u0, T, 0.0001) ≈ u_ref atol = 1e-3
+    end
 end
