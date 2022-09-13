2878dd6733f560d5022f13ff716e152f00d1a2d1
Documenter.jl <documenter@juliadocs.github.io>
2022-09-13 08:29:39 +0000

    build based on 557b8a5

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = AwesomeEquations","category":"page"},{"location":"#AwesomeEquations","page":"Home","title":"AwesomeEquations","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for AwesomeEquations.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [AwesomeEquations]","category":"page"}]
+}
